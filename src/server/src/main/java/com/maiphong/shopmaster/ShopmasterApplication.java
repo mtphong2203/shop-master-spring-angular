12be04042760dd79ab6062e2ac0b36881357ceff
Tien Phong <tienphong2203@gmail.com>
2024-12-15 18:05:30 +0700

    Create base project client and server

---
+++ src/server/src/main/java/com/maiphong/shopmaster/ShopmasterApplication.java
@@ -0,0 +1,13 @@
+package com.maiphong.shopmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopmasterApplication.class, args);
+	}
+
+}
---
+++ src/server/src/test/java/com/maiphong/shopmaster/ShopmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maiphong.shopmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
